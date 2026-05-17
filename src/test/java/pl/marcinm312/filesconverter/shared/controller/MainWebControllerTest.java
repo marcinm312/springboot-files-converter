9599d9f18739112c96c37bf55bb8b4b16b9f0446
marcinm312 <marcin.michalczyk.312@gmail.com>
2026-05-17 20:35:12 +0200

    Adding webjars

--- src/test/java/pl/marcinm312/filesconverter/shared/controller/MainWebControllerTest.java
+++ src/test/java/pl/marcinm312/filesconverter/shared/controller/MainWebControllerTest.java
@@ -1,15 +1,19 @@
 package pl.marcinm312.filesconverter.shared.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.stream.Stream;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,5 +28,24 @@ class MainWebControllerTest {
 				.andDo(print())
 				.andExpect(status().isOk())
 				.andExpect(view().name("mainPage"));
+	}
+
+	@ParameterizedTest
+	@MethodSource("examplesOfStaticResources")
+	void getStaticResource_simpleCase_success(String url, String contentType) throws Exception {
+
+		mockMvc.perform(
+						get(url))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(contentType));
+	}
+
+	private static Stream<Arguments> examplesOfStaticResources() {
+
+		return Stream.of(
+				Arguments.of("/css/style.css", "text/css"),
+				Arguments.of("/webjars/bootstrap/css/bootstrap.min.css", "text/css"),
+				Arguments.of("/js/clearResult.js", "text/javascript")
+		);
 	}
 }
